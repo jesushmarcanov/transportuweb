09730e4864b6056a163c38317012f002fbe5cff5
jesushmarcanov <jesushmarcanov@gmail.com>
2021-04-13 15:17:34 -0400

    Modificaciones form_login

--- frm_login.php
+++ frm_login.php
@@ -9,23 +9,26 @@
                 <h1 class="titulos text-center font-weight-bold">¿Cuál es tu email y tu contraseña?</h1>
                 <form action="">
 					<div class="form-group">
-						<input type="text" class="form-control form-control-lg" placeholder="Nombre" name="nombre" id="nombre">
+						<input type="text" class="inputs form-control form-control-lg" placeholder="Nombre" name="nombre" id="nombre">
 					</div>
 
 					<div class="form-group">
-						<input type="password" class="form-control form-control-lg" placeholder="Contraseña" name="pass" id="pass">
+						<input type="password" class="inputs form-control form-control-lg" placeholder="Contraseña" name="pass" id="pass">
 					</div>
 					
                     <div class="form-check">
 						<label class="form-check-label">
-						<input type="checkbox" name="terminos" id="terminos" class="form-check-input mr-2 align-self-end"> Recordarme 
+						<input type="checkbox" name="terminos" id="terminos" class="form-check-input mr-2"> 
+						<p class="h4 remember mt-1 font-weight-bold">Recordarme</p>
 						</label>
+						
 					</div>
+					<a href="#"><p class="h4 forget mt-3 font-weight-bold">He olvidado mi contraseña</p></a>
 				</form>
             </div>
         </div>
 		<div class="row justify-content-center">
-			<button class="btn btn-primary" type="submit">Enviar</button>
+			<button class="boton btn btn-primary mt-3" type="submit">Enviar</button>
 		</div>
     </div>
 </section>
